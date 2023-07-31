d499df05f51a54ffcfa2ce35fa5a27220e290f9e
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-07-31 20:52:32 +0300

    variables and values = const

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,9 @@
+fun main(){
+//    val const1:String = "Hello, "
+    val constValue1 = "Hello"
+//    var variable1:String = "lola"
+    var variable1 = "lola"
+    variable1="World!"
+    println("const = $constValue1 and variable value = $variable1")
+    println("Sum 2 and 2 equals ${2+2}")
+}
